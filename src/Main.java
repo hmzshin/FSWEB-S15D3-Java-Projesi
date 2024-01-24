e125a2cdc88451a2cda07e4e07f267dd198135d6
hmzshin <hamzasahin@outlook.com>
2024-01-24 13:11:27 +0300

    create employeed and add them to linkedList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee employee1 = new Employee(1, "Hamza", "Şahin");
+        Employee employee2 = new Employee(2, "Ahmet", "Özcan");
+        Employee employee3 = new Employee(3, "Ayşe", "Kartal");
+        Employee employee4 = new Employee(3, "Kemal", "Yurtseven");
+        Employee employee5 = new Employee(4, "Ömer", "Şahin");
+        Employee employee6 = new Employee(4, "Kaya", "Şahin");
+
+        List<Employee> employees = new LinkedList<>();
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee4);
+        employees.add(employee5);
+        employees.add(employee6);
+
+        System.out.println(employees);
+
     }
 }
